5509804e47cc359aa0e256ec9d7a82cd53332eca
NgocTienWork2002 <134667954+NgocTienWork2002@users.noreply.github.com>
2023-12-21 00:21:51 +0700

    Removing constructor in enity ( role,user)

--- src/main/java/com/capstone/gateway/Entities/RoleEntity.java
+++ src/main/java/com/capstone/gateway/Entities/RoleEntity.java
@@ -1,14 +1,10 @@
 package com.capstone.gateway.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
-@Entity
+//@Entity
 public class RoleEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Integer roleId;
-    private String rolename;
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+//    private Integer roleId;
+//    private String rolename;
 }
